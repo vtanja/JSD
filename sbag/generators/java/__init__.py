dac026a38f9b39b6551a9739ef2528784311df48
Balsa Sarenac <balsa.smi15@gmail.com>
2021-03-27 13:25:18 +0100

    Call generator for each entity in model

--- sbag/generators/java/__init__.py
+++ sbag/generators/java/__init__.py
@@ -1,23 +1,30 @@
-import os
-from functools import partial
-from textx import generator, gen_file, get_output_filename
+from os import mkdir
+from os.path import dirname, exists, join
+
+from textx import generator
+from textxjinja import textx_jinja_generator
 
 
 @generator('sbag', 'java')
 def sbag_generate_java(metamodel, model, output_path, overwrite, debug, **custom_args):
     "Generator for generating java from sbag descriptions"
 
-    output_file = get_output_filename(model.file_name, output_path, '*.sbag')
-    gen_file(model.file_name, output_file,
-             partial(generator_callback, model, output_file),
-             overwrite,
-             success_message="""To start Spring Boot application you need maven installed
-             Run mvn clean install
-             Following with java -jar target/<name_of_the_app>.0.0.1.SNAPSHOT.jar""")
+    this_folder = dirname(__file__)
 
+    config = {}
 
-def generator_callback(model, output_file):
-    """
-    A generator function taht produces output_file from model.
-    """
-    # TODO: Write here code taht produce generated output
+    if output_path is None:
+        output_path = join(this_folder, 'gen', '')
+        mkdir(output_path)
+
+    template_folder = join(this_folder, 'templates')
+
+    # Run Jinja generator
+    for entity in model.entities:
+        config['entity'] = entity
+        config['entity_name'] = entity.name
+        entity_path = join(output_path, entity.name, '')
+        if not exists(entity_path):
+            mkdir(entity_path)
+        textx_jinja_generator(template_folder, entity_path, config,
+                              overwrite)

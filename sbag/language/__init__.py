216d6030c4ef004db7c5bfcfb759e53fe2ae7f36
Balša Šarenac <balsa.smi15@gmail.com>
2021-04-12 21:55:33 +0200

    Move builtin classes to separate module

--- sbag/language/__init__.py
+++ sbag/language/__init__.py
@@ -1,91 +1,9 @@
 import os
 from textx import language, metamodel_from_file
+from .builtins import Entity, BaseType, Config, Property, OneToOne, OneToMany, ManyToOne, ManyToMany
 
 current_dir = os.path.dirname(__file__)
 
-
-class BaseType():
-    """
-    Class for defining base types eg. string, double and integer...
-    """
-
-    def __init__(self, parent, name):
-        """Constuctor for instatiating base types."""
-        self.parent = parent
-        self.name = name
-
-    def __str__(self):
-        return self.name
-
-
-class Property():
-    """
-    Defines both base properties and associations.
-    """
-
-    def __init__(self, parent, name, ptype):
-        """Constructor for BaseTypes and Associations properties."""
-        self.parent = parent
-        self.name = name
-        self.ptype = ptype
-
-
-class Entity():
-    """
-    Class for defining entities.
-    """
-
-    def __init__(self, name, parent, properties):
-        """Constructor for Entities."""
-        self.name = name
-        self.parent = parent
-        self.properties = [Property(self, 'id', BaseType(None, 'Long'))]
-        self.properties.extend(properties)
-
-    def __str__(self):
-        return self.name
-
-
-class Config():
-    """
-    Class for defining project configuration.
-    """
-
-    def __init__(self, project, group, description, parent):
-        """Create config instance."""
-        self.project = project
-        self.group = group
-        self.description = description
-        self.parent = parent
-
-
-class OneToOne():
-
-    def __init__(self, parent, option):
-        """One to one entity association type."""
-        self.parent = parent
-        self.option = option
-
-class OneToMany():
-
-    def __init__(self, parent, owner):
-        """One to many entity association type."""
-        self.parent = parent
-        self.owner = owner
-
-class ManyToOne():
-
-    def __init__(self, parent, option):
-        """Many to one entity association type."""
-        self.parent = parent
-        self.option = option
-
-class ManyToMany:
-
-    def __init__(self, parent, owner):
-        """Many to many entity association type."""
-        self.parent = parent
-        self.owner = owner
 
 
 @language('sbag', '*.sbag')

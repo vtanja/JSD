30602da54ee7a8f7be39a5ca6daab14d407cf63b
Tanja <tanja.vukmirovic8@gmail.com>
2021-04-15 13:34:21 +0200

    Added necessary imports for header component

--- sbag/generators/javascript/entities/shared/shared.module.ts
+++ sbag/generators/javascript/entities/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
-
+import { HeaderComponent } from './header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
-  declarations: [LoaderComponent],
+  declarations: [LoaderComponent, HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   exports: [
-    LoaderComponent
+    LoaderComponent,
+    HeaderComponent
   ]
 })
 export class SharedModule { }

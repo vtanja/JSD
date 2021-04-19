75a6bfcd37ccc746c4c0839e82c0d09d713d84dd
Tanja <tanja.vukmirovic8@gmail.com>
2021-04-19 14:02:35 +0200

    Added auth service

---
+++ sbag/generators/javascript/entities/shared/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(data: any) {
+		return this.http.post<any>(environment.api + '/auth/login', data);
+	}
+}

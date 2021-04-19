4f9beb7e2d99af156157876b49f469f27f2c4022
Tanja <tanja.vukmirovic8@gmail.com>
2021-04-19 17:31:39 +0200

    Implemented logout function

--- sbag/generators/javascript/templates/app/src/app/app.component.ts
+++ sbag/generators/javascript/templates/app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from './shared/shared.service';
+import { AuthService } from './shared/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,19 @@ import { SharedService } from './shared/shared.service';
 export class AppComponent implements OnInit {
     title = 'app';
     isLoading: boolean = true;
+    isLogged: boolean = false;
 
-    constructor(private sharedService: SharedService){}
+    constructor(private sharedService: SharedService, private authService: AuthService){
+      this.isLogged = this.authService.getToken() != '' ? true: false;
+    }
 
     ngOnInit() {
       this.sharedService.isLoadingSubject.subscribe((res : any) => {
         this.isLoading = res;
+      });
+
+      this.sharedService.loggedInSubject.subscribe((res: any)=>{
+        this.isLogged = res;
       });
     }
 }
